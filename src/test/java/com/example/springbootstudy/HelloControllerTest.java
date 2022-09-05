32b26388b9231810194560966f74cd2dd4563644
KiKap <hankikab@gamil.com>
2022-09-06 08:04:24 +0900

    - 롬복_기능_테스트

--- src/test/java/com/example/springbootstudy/HelloControllerTest.java
+++ src/test/java/com/example/springbootstudy/HelloControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.springbootstudy;
 
 import com.example.springbootstudy.web.HelloController;
+import com.example.springbootstudy.web.dto.HelloResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,5 +27,16 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void 롬복_기능_테스트() {
+        String name = "test";
+        int amount = 1000;
+
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
     }
 }
